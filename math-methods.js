5a4ca306f643b5125fcd26b4890bc2a518797c1a
mmesander <mark_mesander@hotmail.com>
2023-03-27 22:56:06 +0200

    Math-methods af

--- math-methods.js
+++ math-methods.js
@@ -9,18 +9,41 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+console.log("Opdracht 1:")
+console.log("---------------------")
+
+const circumference1 = Math.PI * 4
+console.log(circumference1)
+
+console.log("---------------------")
+console.log(" ")
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
+console.log("Opdracht 2:")
+console.log("---------------------")
+
+const circumference2 = Math.round(Math.PI * 12)
+console.log(circumference2)
+
+console.log("---------------------")
+console.log(" ")
 
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
+console.log("Opdracht 3:")
+console.log("---------------------")
 
+const root = Math.sqrt(9)
+console.log(root)
+
+console.log("---------------------")
+console.log(" ")
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +51,18 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+console.log("Opdracht 4:")
+console.log("---------------------")
+
+const randomNumber = Math.random()
+if (randomNumber > 0.5) {
+    console.log("Het glas is halfvol")
+    console.log(" ")
+} else {
+    console.log("Het glas is half leeg")
+    console.log(" ")
+}
+
+console.log("---------------------")
+console.log(" ")
